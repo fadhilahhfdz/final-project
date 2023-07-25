04018bd8f2f43d926c8323f76a94c68c83f1f996
fadill1 <125537223+fadill1@users.noreply.github.com>
2023-07-25 13:59:21 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function MyAlert() {
+    alert('Ubah password berhasil\nSilahkan login kembali')
+}
+
+function Register() {
+    alert('Akun berhasil dibuat\nSilahkan login menggunakan email dan password')
+}
